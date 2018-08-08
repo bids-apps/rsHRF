4e89fefbf826fb2a4d6c0423e2da5ba33d92d99b
Madhur Tandon <madhur16053@iiitd.ac.in>
2018-08-08 18:04:22 +0530

    Ensure the updated image has tag latest

--- rsHRF/CLI.py
+++ rsHRF/CLI.py
@@ -5,7 +5,7 @@ import numpy as np
 import warnings
 from rsHRF import spm_dep, fourD_rsHRF
 
-__version__ = "0.2.8"
+__version__ = "0.2.9"
 
 def get_parser():
     parser = ArgumentParser(description='retrieves the onsets of pseudo-events triggering a '
